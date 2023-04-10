f1cc8e4435e434a97e3a58f44cf6aef92c6f36df
Omar-AT-Hashem <omar.at.hashem@gmail.com>
2023-04-10 23:43:38 +0200

    ficed front end bug not sending the right authorization with the request

--- client/src/pages/userPages/updateUserInfo/apiHandler.js
+++ client/src/pages/userPages/updateUserInfo/apiHandler.js
@@ -1,52 +1,52 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: `http://localhost:5000/api`,
-    headers: { 
-        'Access-Control-Allow-Origin': '*', 
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS', 
-        'Access-Control-Allow-Headers': 'append,delete,entries,foreach,get,has,keys,set,values,Authorization', 
-        'Content-Type': 'application/json',
-    }
-})
+  baseURL: `http://localhost:5000/api`,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    "Access-Control-Allow-Headers":
+      "append,delete,entries,foreach,get,has,keys,set,values,Authorization",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+    userid: `${localStorage.getItem("userID")}`,
+  },
+});
 
 export class apiHandler {
-
-
-    async verifyUser(user)  {
-        try{
-        let response =  await api.post('/login', user);
-        return response
-        }catch(err){
-            return err.response
-        }
-
-}
-
-async updateUser(user)  {
-    try{
-    let response =  await api.put(`/user/update-data/${user.id}`, user);
-    return response
-    }catch(err){
-        return err.response
+  async verifyUser(user) {
+    try {
+      let response = await api.post("/login", user);
+      return response;
+    } catch (err) {
+      return err.response;
     }
-}
+  }
 
-async updateUserPassword(user) {
-    try{
-        let response =  await api.put(`/user/update-password/${user.id}`, user);
-        return response
-        }catch(err){
-            return err.response
-        }
-}
-
-async deleteUser(userID)  {
-    try{
-    let response =  await api.delete(`/user/delete/${userID}`);
-    return response
-    }catch(err){
-        return err.response; 
+  async updateUser(user) {
+    try {
+      let response = await api.put(`/user/update-data/${user.id}`, user);
+      return response;
+    } catch (err) {
+      return err.response;
     }
-}
+  }
+
+  async updateUserPassword(user) {
+    try {
+      let response = await api.put(`/user/update-password/${user.id}`, user);
+      return response;
+    } catch (err) {
+      return err.response;
+    }
+  }
+
+  async deleteUser(userID) {
+    try {
+      let response = await api.delete(`/user/delete/${userID}`);
+      return response;
+    } catch (err) {
+      return err.response;
+    }
+  }
 }
